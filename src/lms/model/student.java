2870a231f03af23df4276fc4c31c809e1ec31687
aditya <aditya@192.168.0.2>
2017-09-05 14:43:24 +0530

    added model

---
+++ src/lms/model/student.java
@@ -0,0 +1,27 @@
+package lms.model;
+
+public class student {
+	
+	private int rollno;
+	private String name;
+	private String course;
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+}
